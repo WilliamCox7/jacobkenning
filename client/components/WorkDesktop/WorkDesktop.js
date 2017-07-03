f4d960a389da50037887fb09add87d475132556c
William Cox <willubyu7@gmail.com>
2017-07-03 11:27:32 -0600

    added next/prev buttons to desktop

--- client/components/WorkDesktop/WorkDesktop.js
+++ client/components/WorkDesktop/WorkDesktop.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WorkPostDesktop from './WorkPostDesktop';
+import prevarrow from '../../src/prev-arrow.svg';
 import nextarrow from '../../src/next-arrow.svg';
 import './WorkDesktop.scss';
 
@@ -8,11 +9,28 @@ class WorkDesktop extends Component {
   constructor(props) {
     super(props);
     this.selectPost = this.selectPost.bind(this);
+    this.prev = this.prev.bind(this);
+    this.next = this.next.bind(this);
   }
 
-  selectPost(post) {
+  selectPost(post, i) {
+    this.props.updWorkIndex(i);
     this.props.storeWork(post);
     this.props.goHome(false);
+  }
+
+  prev() {
+    var newIndex = this.props.workIndex-1;
+    this.props.updWorkIndex(newIndex);
+    this.props.storeWork(this.props.works[newIndex]);
+    window.scroll(0, 0)
+  }
+
+  next() {
+    var newIndex = this.props.workIndex+1;
+    this.props.updWorkIndex(newIndex);
+    this.props.storeWork(this.props.works[newIndex]);
+    window.scroll(0, 0)
   }
 
   render() {
@@ -50,7 +68,7 @@ class WorkDesktop extends Component {
               {initHeader}
             </div>
             <p className="init-para">{initPara.substring(0, 350)}...</p>
-            <p onClick={() => { this.selectPost(post); }} className="case-link">
+            <p onClick={() => { this.selectPost(post, i); }} className="case-link">
               See case study
               <img src={nextarrow} />
             </p>
@@ -64,6 +82,26 @@ class WorkDesktop extends Component {
         {this.props.isHome ? (posts) : (
           <WorkPostDesktop work={this.props.curWork} />
         )}
+        {location !== "/about" && !this.props.isHome ? (
+          <div className="nav-arrows">
+            <div className="arrow">
+              {this.props.workIndex > 0 ? (
+                <span onClick={this.prev}>
+                  <img src={prevarrow} />
+                  <p>prev</p>
+                </span>
+              ) : (null)}
+            </div>
+            <div className="arrow">
+              {this.props.workIndex < this.props.works.length-1 ? (
+                <span onClick={this.next}>
+                  <p>next</p>
+                  <img src={nextarrow} />
+                </span>
+              ) : (null)}
+            </div>
+          </div>
+        ) : (null)}
       </section>
     )
   }
